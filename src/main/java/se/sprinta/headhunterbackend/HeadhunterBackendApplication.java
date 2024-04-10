729061fdd734a965438bf7f74cf13266981de17c
MikaelEngvall <mikael@engvall.org>
2024-04-10 15:53:30 +0200

    Minor

--- src/main/java/se/sprinta/headhunterbackend/HeadhunterBackendApplication.java
+++ src/main/java/se/sprinta/headhunterbackend/HeadhunterBackendApplication.java
@@ -1,12 +1,10 @@
 package se.sprinta.headhunterbackend;
 
 // Importing necessary libraries
+
 import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 // Log4j2 annotation for logging
 @Log4j2

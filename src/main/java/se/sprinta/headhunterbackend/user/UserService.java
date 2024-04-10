e1731ba8d53ab4268ad21f9f8634ff41c6054f9c
MikaelEngvall <mikael@engvall.org>
2024-04-10 15:38:13 +0200

    Documented UserService

--- src/main/java/se/sprinta/headhunterbackend/user/UserService.java
+++ src/main/java/se/sprinta/headhunterbackend/user/UserService.java
@@ -11,9 +11,11 @@ import se.sprinta.headhunterbackend.system.exception.ObjectNotFoundException;
 import java.util.List;
 
 /**
- * Business logic for Job
+ * This class is responsible for handling business logic related to User.
+ * It uses Spring's @Service annotation to mark it as a service that holds business logic.
+ * It uses @Transactional annotation to ensure that all methods within this class are wrapped with a transaction.
+ * It implements UserDetailsService interface which is a core interface in Spring Security framework, used to retrieve the user’s authentication and authorization information.
  */
-
 @Service
 @Transactional
 public class UserService implements UserDetailsService {
@@ -21,47 +23,59 @@ public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    /**
+     * Constructor for UserService.
+     * It initializes userRepository and passwordEncoder.
+     */
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * This method is used to fetch all users.
+     */
     public List<User> findAll() {
         return this.userRepository.findAll();
     }
 
+    /**
+     * This method is used to fetch a user by email.
+     */
     public User findByUserEmail(String email) {
         return this.userRepository.findByEmail(email)
                 .orElseThrow(() -> new ObjectNotFoundException("user", email));
     }
 
+    /**
+     * This method is used to save a new user.
+     * It encodes the password before saving the user.
+     */
     public User save(User newUser) {
         newUser.setPassword(this.passwordEncoder.encode(newUser.getPassword()));
         return this.userRepository.save(newUser);
     }
 
     /**
-     * Updates a user. Certain precautions has to be made for the roles.
-     *
-     * @param email  Is used to find the User object that we want to update.
-     * @param update Holds potentially both username and roles, or at least one of them.
-     *               Any double quotes are being removed by the logic.
-     * @return The updated User object.
+     * This method is used to update a user.
+     * It finds the user by email and updates the username and roles.
+     * Any double quotes in the roles are being removed by the logic.
      */
-
     public User update(String email, User update) {
         User foundUser = this.userRepository.findByEmail(email)
                 .orElseThrow(() -> new ObjectNotFoundException("user", email));
 
         foundUser.setUsername(update.getUsername());
 
-        // TODO: 14/03/2024 Is this replace still needed?
         String rolesFixed = update.getRoles().replace("\"", "");
         foundUser.setRoles(rolesFixed);
         return this.userRepository.save(foundUser);
     }
 
-
+    /**
+     * This method is used to delete a user.
+     * It finds the user by email and deletes it.
+     */
     public void delete(String email) {
         User foundUser = this.userRepository.findByEmail(email)
                 .orElseThrow(() -> new ObjectNotFoundException("user", email));
@@ -69,18 +83,15 @@ public class UserService implements UserDetailsService {
     }
 
     /**
-     * UserDetails is fetched to Spring Security to check authentication.
-     *
-     * @param email Is used to find the User object that tries to log in.
-     * @return UserDetails Here are the credentials that are being matched with the provided username and password by the user when trying to log in.
-     * @throws UsernameNotFoundException If the User object doesn't exist, this exception is being thrown.
+     * This method is used to fetch UserDetails for Spring Security to check authentication.
+     * It finds the user by email and wraps the returned user instance in a MyUserPrincipal instance.
+     * If the User object doesn't exist, UsernameNotFoundException is thrown.
      */
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        UserDetails userDetails = this.userRepository.findByEmail(email) // First, we need to find this user from database.
-                .map(MyUserPrincipal::new) // If found, wrap the returned user instance in a MyUserPrincipal instance.
-                .orElseThrow(() -> new UsernameNotFoundException("email " + email + " is not found")); // Otherwise, throw an exception.
+        UserDetails userDetails = this.userRepository.findByEmail(email)
+                .map(MyUserPrincipal::new)
+                .orElseThrow(() -> new UsernameNotFoundException("email " + email + " is not found"));
         return userDetails;
     }
 }
